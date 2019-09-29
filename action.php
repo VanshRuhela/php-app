98a5bc5bf45d11db4515f71f7045f11c31715e62
VanshRuhela <55937349+VanshRuhela@users.noreply.github.com>
2019-09-29 10:53:19 +0530

    fix

--- action.php
+++ action.php
@@ -14,31 +14,31 @@ if(!empty($name)|| !empty($contact)|| !empty($En)|| !empty($branch)|| !empty($in
     $conn=new mysqli($severname,$username,$password,$dbname);
     $room;
     
-        if($interest==I)
+        if($interest=='I')
         { $a="SELECT count(*) FROM student HAVING intrest='I' ";$a++; 
-        $room=100+$a;
+        $room=100+(int)$a;
         }
-      elseif($interest==C)
+      elseif($interest=='C')
         { $b="SELECT count(*) FROM student WHERE intrest='C' ";$b++;
-        $room=200+$b;
+        $room=200+(int)$b;
         }
-         elseif($interest==S)
+         elseif($interest=='S')
         { $c="SELECT count(*) FROM student WHERE intrest='S' ";$c++;
-        $room=300+$c;
+        $room=300+(int)$c;
         }
-       elseif($interest==R)
+       elseif($interest=='R')
         { $d="SELECT count(*) FROM student WHERE intrest='R' ";$d++;
-        $room=400+$d;
+        $room=400+(int)$d;
         }
          else
         { $e="SELECT count(*) FROM student WHERE intrest='G' ";$e++;
-        $room=500+$e;
+        $room=500+(int)$e;
         }
     
     $sql="INSERT INTO student ( name ,  Contact ,  En ,  branch ,  intrest , RoomNo ) VALUES ('".$name."','".$contact."','".$En."','".$branch."','".$interest."','".$room."');";
     if (!mysqli_query($conn,$sql))
       {
-      echo("Error description: " . mysqli_error($con));
+      echo("Error description: " . mysqli_error($conn));
       }
 }
 else{
